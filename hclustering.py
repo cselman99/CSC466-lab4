d3ab78d9bd81c254dcba7a97f1d7f64996251ad8
carterselman99@gmail.com <carterselman99@gmail.com>
2021-10-31 17:27:12 -0700

    Getting close. Still not classifying all correctly

--- hclustering.py
+++ hclustering.py
@@ -5,14 +5,30 @@ from calculations import distance, normalize
 import matplotlib.pyplot as plt
 
 
+class Node:
+    value = None
+    child1 = None
+    child2 = None
+    dist = None
+
+    def __init__(self, value, c1: 'Node' = None, c2: 'Node' = None, dist: float = 0):
+        self.value = value
+        self.child1 = c1
+        self.child2 = c2
+        self.dist = dist
+
+    def __repr__(self):
+        return str(self.value) + " dist: " + str(self.dist) + " " + str(self.child1) + " , " + str(self.child2)
+
+
+
 # should use average link in most cases
 # single link extends in weird directions
 # complete link builds spheres
 
 def h_clustering(points, threshold):
-    clusters = [[p] for p in points.tolist()]
+    clusters = [Node([p]) for p in points.tolist()]
     distance_arr = np.zeros((len(clusters), len(clusters)), dtype=float)
-    cluster_tracker = {}
     while len(distance_arr) > 1:
         min_dist = None
         c1 = None
@@ -22,36 +38,48 @@ def h_clustering(points, threshold):
                 if i == j:
                     continue
                 # Only calculate distance if dist_arr value has not been previously calculated
-                distance_arr[i, j] = distance(clusters[i], clusters[j]) if distance_arr[i, j] == 0 else distance_arr[i, j]
+                distance_arr[i, j] = distance(clusters[i].value, clusters[j].value) \
+                    if distance_arr[i, j] == 0 else distance_arr[i, j]
                 cur_dist = distance_arr[i, j]
                 if min_dist is None or cur_dist < min_dist:
                     min_dist = cur_dist
                     c1 = i
                     c2 = j
         # Merge C1 and C2 clusters
-        clusters[c1] += clusters[c2]
-        cluster_tracker[min_dist] = clusters[c1]
+        new_node = Node(clusters[c1].value + clusters[c2].value, clusters[c1], clusters[c2], min_dist)
+        clusters[c1] = new_node
+        # Done using c2 point, now we can pop
+        clusters.remove(clusters[c2])
 
         # Calculate minimum distance values between merged points
         for t in range(len(distance_arr[c1])):
             if t != c1 and t != c2:
                 distance_arr[c1][t] = min(distance_arr[c1][t], distance_arr[c2][t])
 
-        # print(str(len(distance_arr)) + " x " + str(len(distance_arr[0])))
         # Remove c2 row and column
         distance_arr = np.delete(distance_arr, c2, axis=0)
         distance_arr = np.delete(distance_arr, c2, axis=1)
-        # Done using c2 point, now we can pop
-        clusters.pop(c2)
 
-    final = []
-    for dist, cluster in cluster_tracker.items():
-        if dist <= threshold:
-            final.append(cluster)
-    # print(final)
+    final_list = []
+    get_threshold_nodes(clusters[0], threshold, final_list)
+    # for dist, cluster in dendrogram.items():
+    #     if dist <= threshold:
+    #         final.append(cluster)
     # need to consider single points that are left out
-    return final
+    return final_list
 
+def get_threshold_nodes(root, threshold, final_list):
+    if root is None:
+        return
+    if root.dist == 0:  # Found a leaf node
+        final_list.append(root.value)
+        return
+    if root.dist > threshold:
+        get_threshold_nodes(root.child1, threshold, final_list)  # Check right
+        get_threshold_nodes(root.child2, threshold, final_list)  # Check left
+    else:
+        final_list.append(root.value)
+    return
 
 if __name__ == "__main__":
     if len(sys.argv) == 2:
@@ -59,20 +87,14 @@ if __name__ == "__main__":
         data = parse.parseData(filename)
         data_norm = normalize(np.asarray(data, dtype=float))  # possible issue with normalization
 
-        final_clusters = h_clustering(data_norm, 1.05)
-        # print("Final cluster length: " + str(len(final_clusters)))
-        # colors = []
-        # for i in range(len(final_clusters)):
-        #     colors.append([(i + 1) / len(final_clusters) for j in range(len(final_clusters[i]))])
-        colors = ['red', 'green', '#88c999', "blue", "gray", "cyan", "magenta"]
-        # print(len(final_clusters))
+        final_clusters = h_clustering(data_norm, 1.4)
+        colors = ["red","green","blue","yellow","pink","black","orange","purple","beige","brown","gray","cyan","magenta"]
         for i, c in enumerate(final_clusters):
             x_coords = []
             y_coords = []
-            for x, y in c:
-                x_coords.append(x)
-                y_coords.append(y)
-            # print(colors)
+            for coords in c:
+                x_coords.append(coords[0])
+                y_coords.append(coords[1])
             x_coords = np.asarray(x_coords)
             y_coords = np.asarray(y_coords)
             plt.scatter(x_coords, y_coords, color=colors[i % len(colors)])
--- parse.py
+++ parse.py
@@ -2,13 +2,15 @@ def parseData(input_file):
     file = open(input_file, "r")
     lines = file.readlines()
     d = []
-    restrictions = lines[0].split(",")
+    restrictions = [int(li.strip()) for li in lines[0].split(",")]
     lines = lines[1:]
-    for i, line in enumerate(lines):
-        if restrictions[i] == 0:
-            continue
+    for line in lines:
         line = line.strip().replace("\"", "")
         lineList = line.split(",")
         if len(lineList) > 1:
-            d += [lineList]
+            new_lines = []
+            for li in range(len(lineList)):
+                if restrictions[li] == 1:
+                    new_lines.append(lineList[li])
+            d += [new_lines]
     return d
